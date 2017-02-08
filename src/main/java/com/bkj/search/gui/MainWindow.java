3d27dec98cdb75b19dddc3ebcb4e971c4efe2a5d
Bradley Claus <xinnx009@gmail.com>
2017-02-08 00:38:14 -0500

    Refactored MainWindow to make it clearer

--- src/main/java/com/bkj/search/gui/MainWindow.java
+++ src/main/java/com/bkj/search/gui/MainWindow.java
@@ -8,9 +8,36 @@ import java.awt.*;
  */
 
 public class MainWindow implements Runnable {
+    // TODO: We should make our own versions of these
+    JFrame frame;
+    JPanel mainPane, searchPane, resultsPane;
+    JLabel searchFieldLabel;
+    JTextField queryTextField;
+    JButton querySearchButton;
 
     public MainWindow() {
+        frame = new JFrame("SearchUI");
+        mainPane = new JPanel(new BorderLayout());
+        searchPane = new JPanel(new FlowLayout());
+        resultsPane = new JPanel(new FlowLayout());
 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(new Rectangle(640,480));
+
+        searchFieldLabel = new JLabel("Query: ");
+        searchPane.add(searchFieldLabel);
+
+        queryTextField = new JTextField(20);
+        searchPane.add(queryTextField);
+
+        querySearchButton = new JButton("Begin Search");
+        searchPane.add(querySearchButton);
+
+        // Display Window
+        mainPane.add(searchPane);
+
+        mainPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(mainPane);
     }
 
     @Override
@@ -19,24 +46,7 @@ public class MainWindow implements Runnable {
     }
 
     private void showInterface() {
-        JFrame frame = new JFrame("SearchUI");
-        JPanel mainPane = new JPanel(new BorderLayout());
-        JPanel searchPane = new JPanel(new FlowLayout());
-        JPanel resultsPane = new JPanel(new FlowLayout());
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(new Rectangle(640,480));
-
-        JLabel label_hw = new JLabel("Search: ");
-        JTextField textBox = new JTextField(20);
-        searchPane.add(label_hw);
-        searchPane.add(textBox);
-
-        // Display Window
-        mainPane.add(searchPane);
-
-        mainPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(mainPane);
         frame.setVisible(true);
     }
 }
