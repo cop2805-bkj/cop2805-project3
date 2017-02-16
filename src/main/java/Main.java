a8b0109fa0076a9812ca48b41adf2e6f088f8eec
Jonathan Llaneras <jonathanl1020.jl@gmail.com>
2017-02-16 09:35:12 -0500

    Adding Comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.bkj.search.gui.MainWindowFrame;
 /**
  * Created by bclaus on 2/7/17.
  */
+// Displays UI
 public class Main {
     public static void main(String[] args){
         // In future versions we can handle different UI's for example from here
