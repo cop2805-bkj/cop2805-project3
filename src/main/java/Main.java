fb0ffdf0071f82cf8e0936708b032d7befd96fc8
Bradley Claus <xinnx009@gmail.com>
2017-02-28 12:57:19 -0500

    Basic builder implemented for MainWindow
    The easiest approach for now is to do our configuration in builder
    objects. I am planning on using Gson to write/read a builder object
    from disk as a configuation file.
    
    I would like to store our inverted tree's in a better format however
    i.e a database or something similar so we don't have to reindex on
    startup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 import com.bkj.search.gui.MainWindow;
 
-/**
- * Created by bclaus on 2/7/17.
- */
 // Displays UI
 public class Main {
     public static void main(String[] args){
         // In future versions we can handle different UI's for example from here
-        javax.swing.SwingUtilities.invokeLater(new MainWindow());
+        javax.swing.SwingUtilities.invokeLater(new MainWindow.MainWindowBuilder().build());
     }
 }
