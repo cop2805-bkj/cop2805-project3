818cffd75ee68116b2b40eb561ee840d5c0d28a5
Bradley Claus <xinnx009@gmail.com>
2017-03-06 11:54:10 -0500

    Started implementation of a inverted index

---
+++ src/main/java/com/bkj/search/gui/InvertedIndexMap.java
@@ -0,0 +1,19 @@
+package com.bkj.search.gui;
+
+import sun.reflect.Reflection;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by bclaus on 3/6/17.
+ */
+public class InvertedIndexMap<K, V> extends AbstractMap<K, V> {
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
---
+++ src/main/java/com/bkj/search/gui/MD5Checksum.java
@@ -0,0 +1,7 @@
+package com.bkj.search.gui;
+
+/**
+ * Created by bclaus on 3/6/17.
+ */
+public class MD5Checksum {
+}
