99024cca8a2728f8e8627fb7be6a2ab1a8f88285
Jonathan Llaneras <jonathanl1020.jl@gmail.com>
2017-03-19 11:35:02 -0400

    Jonathan Llaneras, Added comments and took out unused imports.

--- src/main/java/GUI/Main_UI.java
+++ src/main/java/GUI/Main_UI.java
@@ -163,9 +163,6 @@ public class Main_UI extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_searchButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
