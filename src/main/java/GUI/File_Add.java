d5349951bd5ff41263c2a6e08df30ab874ce4c9b
Jonathan Llaneras <jonathanl1020.jl@gmail.com>
2017-03-19 11:20:59 -0400

    Jonathan Llaneras, Made a separate class for adding files to the text area and added single file removal.

--- src/main/java/GUI/File_Add.java
+++ src/main/java/GUI/File_Add.java
@@ -10,10 +10,16 @@ import javax.swing.JOptionPane;
  */
 public abstract class File_Add implements Runnable {
     
+    static String pathName = " ";
+    
+    public String pathName() {
+        String pathNameReturn = " ";
+        pathNameReturn = pathName.trim();
+        return pathNameReturn;
+    }
+    
     @Override
     public void run() {
-        Files_UI files = new Files_UI();
-        
         JFileChooser chooser = new JFileChooser();
         chooser.setCurrentDirectory(new java.io.File("Users"));
         chooser.setDialogTitle("choosertitle");
@@ -23,12 +29,11 @@ public abstract class File_Add implements Runnable {
         if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             File file;
             file = chooser.getSelectedFile();
-            String pathName;
             pathName = file.toString();
             System.out.println(pathName);
-            //filesTextArea.setText(pathName);
+            //Files_UI.filesTextArea.append(pathName + "\n");
         } else if (chooser.showOpenDialog(null) == JFileChooser.ERROR_OPTION){
             JOptionPane.showMessageDialog(null,"ALERT MESSAGE","TITLE",JOptionPane.WARNING_MESSAGE);
         }
-    }   
+    }
 }
