82896819e17acd62b54bcb1dd0bf145658067207
Jonathan Llaneras <jonathanl1020.jl@gmail.com>
2017-04-04 21:11:38 -0400

    Jonathan Llaneras, Index almost working it looks through the embedded text file for the file to search through.

--- src/main/java/GUI/Inverted_Index.java
+++ src/main/java/GUI/Inverted_Index.java
@@ -1,19 +1,108 @@
 package GUI;
 
-import java.util.TreeMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
  * @author Jonathan Llaneras
  */
-public class Inverted_Index {
+public class Inverted_Index implements Runnable {
     
-    public static void main(String[] args) {
-        TreeMap<String, Integer> data = new TreeMap<>();
-        
-        
+    private String searchWord;
+    
+    public String getWord() {
+        return searchWord;       
     }
     
-    //public static int get 
+    public void setWord(String word) {
+        searchWord = word;
+    }
     
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        File file =new File("list.txt");
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+            while(in.hasNext())
+            {
+                String line=in.nextLine();
+                if(line.contains(""))
+                    System.out.println("1");
+                File fileText;
+                fileText = new File(line);
+                Scanner readText = null;
+                try {
+                    readText = new Scanner(file);
+                    while(in.hasNext())
+                    {
+                        String lineText=in.nextLine();
+                        lineText = lineText.toLowerCase();
+                        System.out.println("2");
+                        while(lineText.contains(searchWord))
+                            System.out.println(searchWord);
+                    }
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+        }
+    
+    }
+        /*File file =new File("line.txt");
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+            while(in.hasNext())
+            {
+                String line=in.nextLine();
+                if(line.contains(""))
+                    System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+
+        }
+    }*/
+
+    /**
+     *
+     */
+    public void getfiles() {
+        File file =new File("list.txt");
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+            while(in.hasNext())
+            {
+                String line=in.nextLine();
+                if(line.contains(""))
+                    System.out.println(line);
+                File fileText;
+                fileText = new File(line);
+                Scanner readText = null;
+                try {
+                    readText = new Scanner(file);
+                    while(in.hasNext())
+                    {
+                        String lineText=in.nextLine();
+                        System.out.println(lineText);
+                        while(lineText.contains(searchWord))
+                            System.out.println(searchWord);
+                    }
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                }
+            }
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+        }
+    }
 }
