e0337615785fd8bf44a0afacbb6336f65fe9bca7
Jonathan Llaneras <jonathanl1020.jl@gmail.com>
2017-04-06 19:07:23 -0400

    Jonathan Llaneras, Added back main class call to MainWindowFrame...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import com.bkj.search.gui.MainWindowFrame;
 import GUI.Main_UI;
 
 /**
+ * Created by bclaus on 2/7/17.
  * Created by Jonathan Llaneras on 3.18.17
  */
 public class Main {
     public static void main(String[] args){
+        // In future versions we can handle different UI's for example from here
+        javax.swing.SwingUtilities.invokeLater(new MainWindowFrame());
+        
         
         java.awt.EventQueue.invokeLater(() -> {
             Main_UI userInterface = new Main_UI();
